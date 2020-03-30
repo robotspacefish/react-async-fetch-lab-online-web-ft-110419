1ff66d600910a19175da3818c92113a2cb9d61b2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-30 16:29:41 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class myComponent extends Component {
+  state = {
+    people: []
+  };
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(json => this.setState({ people: json.people }))
+      .catch(error => console.log(error.message))
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.people.map(p => <li>{p.name} - {p.craft}</li>)}
+      </ul>
+    );
+  }
+}
